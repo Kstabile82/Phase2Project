c426fe35ac88f3e6f80f47bd0c4d1a67972e7a77
KarinaStabile <karina.stabile@gmail.com>
2022-02-12 19:36:46 -0500

    added no matches pop up to search

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import LikeBtn from "./LikeBtn";
 
-//fix card so it reopens if clicked right after closing, toggle isShowing somehow
+//fix card so it reopens if clicked right after closing
+//need to be able to pull up cards after first one is closed in searchexercises
+
 function Card({ exercise }) {
     const [isShowing, setIsShowing] = useState(true);
     const card = {
@@ -9,19 +11,19 @@ function Card({ exercise }) {
         backgroundColor: "white",
         border: "dotted", 
         font: "red"
-    }
+    } 
     function handleClick(e) {
         e.preventDefault();
         setIsShowing(false);
     }
+
  return (
-    // <div className="card" id={exercise.name} style={{display: isShowing ? 'visible' : 'none'}}>
-    <div className="card" id={exercise.name} style={card}>
+    <div className="card" id={exercise.name} style={card}> 
          <p>Name: {exercise.name}</p>
          <p>Category: {exercise.category.charAt(0).toUpperCase() + exercise.category.slice(1)}</p>
-         <p>Difficulty: {exercise.difficulty.charAt(0).toUpperCase() + exercise.difficulty.slice(1)}</p>
+         <p>Difficulty: {exercise.difficulty.charAt(0).toUpperCase() + exercise.difficulty.slice(1)}</p> 
          <LikeBtn match={exercise} />
-         <button onClick={handleClick}>close</button>
+         <button onClick={handleClick}>close</button>    
      </div>
  )
 }
--- src/Components/SearchExercises.js
+++ src/Components/SearchExercises.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "./Card";
+import Dashboard from "./Dashboard";
 
-function SearchExercises() {
 
+function SearchExercises({ exercises }) {
+const [exerciseName, setExerciseName] = useState("");
+const noMatches = "No matches came up in this search, but feel free to add the exercise to our database!"
+const [result, setResult] = useState("");
+ function handleSubmit(e) {
+     e.preventDefault();
+     let res = exercises.find(ex => ex.name.toLowerCase() === exerciseName.toLowerCase());
+     setResult(res);
+     if (res === undefined) {
+         setResult("none")
+     }
+    }  
     return (
-        <div>Search for an exercise : name here
-
+        <div>Search for an exercise: 
+            <form className="search" onSubmit={handleSubmit}> 
+                <input 
+                type="text" 
+                id="inputext" 
+                placeholder="Exercise name"
+                onChange={(e) => setExerciseName(e.target.value)}></input>  
+                <button>Enter</button>
+            </form>
+            {result !== "" && result !== "none" ? <Card exercise={result} /> : null}
+            {result === "none" ? <Dashboard theText={noMatches} /> : null }
         </div>
     )
 }
-//ability to search exercises, returns card for that exercise
 export default SearchExercises; 
